03a7a4b561e6a694b73f0bd92b84ba4ce1f983c3
Gabriel Simon <gabrielsimon775@gmail.com>
2022-04-21 14:29:18 -0300

    Test using mock, simuling a exception

--- src/test/java/FinalizarLeilaoServiceTest.java
+++ src/test/java/FinalizarLeilaoServiceTest.java
@@ -2,6 +2,7 @@ import br.com.alura.leilao.dao.LeilaoDao;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.service.EnviadorDeEmails;
 import br.com.alura.leilao.service.FinalizarLeilaoService;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,10 +22,13 @@ public class FinalizarLeilaoServiceTest {
     @Mock
     private LeilaoDao leilaoDao;
 
+    @Mock
+    private EnviadorDeEmails  enviadorDeEmails;
+
     @BeforeEach
     public void beforeEach() {
         MockitoAnnotations.initMocks(this);
-        this.service = new FinalizarLeilaoService(leilaoDao);
+        this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
     }
 
     @Test
@@ -40,6 +44,34 @@ public class FinalizarLeilaoServiceTest {
         Assert.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
 
         Mockito.verify(leilaoDao).salvar(leilao);
+    }
+
+    @Test
+    void deveriaEnviarUmEmailParaVencedorDoLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+        Lance leilaoVencedor = leilao.getLanceVencedor();
+
+        Mockito.verify(enviadorDeEmails).enviarEmailVencedorLeilao(leilaoVencedor);
+    }
+
+    @Test
+    void naoDeveriaEnviarUmEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when(leilaoDao.buscarLeiloesExpirados()).thenReturn(leiloes);
+
+        Mockito.when(leilaoDao.salvar(Mockito.any())).thenThrow(RuntimeException.class);
+
+        try {
+            service.finalizarLeiloesExpirados();
+            Mockito.verifyNoInteractions(enviadorDeEmails);
+        } catch (Exception e) { }
     }
 
     private List<Leilao> leiloes() {
